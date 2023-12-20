4795a7a99fc6b07d827de5cff0dc64532f25b515
Nerya Manassen <neryamanassen9@gmail.com>
2023-12-20 07:41:28 +0200

    daily update

---
+++ largestOddNumber.py
@@ -0,0 +1,8 @@
+def largestOddNumber( num: str) -> str:
+    for i in range(len(num) - 1, -1, -1):
+        if int(num[i]) % 2 != 0:
+            return num[:i+1]
+
+    return ""
+num = '52'
+print(largestOddNumber(num))
