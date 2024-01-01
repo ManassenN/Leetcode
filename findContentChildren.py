bb2e6d12b8655365c18545ff7d72c6a7b298cd48
Nerya Manassen <neryamanassen9@gmail.com>
2024-01-01 07:57:02 +0200

    Minor Changes

--- findContentChildren.py
+++ findContentChildren.py
@@ -1,8 +1,10 @@
 
 def findContentChildren(self, g: List[int], s: List[int]) -> int:
+    # Sort with built-in function with complexity of O(nlogn).
     g.sort(reverse = True)
     s.sort(reverse = True)
     res, i , j = 0 ,0,0
+    # Traverse the array with O(n) running time.
     while i<len(g) and j<len(s):
         if g[i]<=s[j]:
             res+=1
