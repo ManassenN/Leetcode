4dae309aa0a34e4dabd4b0f2886c7c16f5154212
Nerya Manassen <neryamanassen9@gmail.com>
2024-01-09 08:02:59 +0200

    daily commit 2

--- leafSimilar.py
+++ leafSimilar.py
@@ -1,21 +1,28 @@
-def leafSimilar(root1,root2):
-    first_tree_leafs,second_tree_leafs=[],[]
-    def dfs(node,leafs):
-        if not node:
-            return 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        first_tree_leafs,second_tree_leafs=[],[]
+        def dfs(node,leafs):
+            if not node:
+                return 
 
-        # This row means that we are on a leaf
-        if not node.left and not node.right:
-            leafs.append(node.val)
-        
-        dfs(node.left,leafs)
-        dfs(node.right,leafs)
+            # This row means that we are on a leaf
+            if not node.left and not node.right:
+                leafs.append(node.val)
+            
+            dfs(node.left,leafs)
+            dfs(node.right,leafs)
 
-        return leafs
-    first_tree_leafs = dfs(root1,[])
-    second_tree_leafs = dfs(root2,[])
+            return leafs
+        first_tree_leafs = dfs(root1,[])
+        second_tree_leafs = dfs(root2,[])
 
-    if first_tree_leafs == second_tree_leafs:
-        return True
-    else:
-        return False
+        if first_tree_leafs == second_tree_leafs:
+            return True
+        else:
+            return False        
