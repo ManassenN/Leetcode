ec9f1e0a3fdaea3b9c64fe4783460bbafbba81fc
Nerya Manassen <neryamanassen9@gmail.com>
2024-01-31 08:47:21 +0200

    daily commit

---
+++ dailyTemperatures.py
@@ -0,0 +1,15 @@
+def dailyTemperature(temperatures):
+  n = len(temperatures)
+  answer = [0] * n
+  stack = []
+
+  for i in range(n):
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            prev_index = stack.pop()
+            answer[prev_index] = i - prev_index
+
+        stack.append(i)
+
+  return answer
+
+dailyTemperature([73,74,75,71,69,72,76,73])
